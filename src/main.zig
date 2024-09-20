93bda632ada8d51a4f86b825614c2f5adaa15c9e
wizard-lgtm <rippiten23@gmail.com>
2024-09-20 00:47:40 +0000

    asm_sum function added sys_write fixed

--- src/main.zig
+++ src/main.zig
@@ -3,40 +3,45 @@ const std = @import("std");
 fn asm_example() usize {
     var result: usize = 0;
     const param: usize = 15;
-    asm volatile (
-        \\mv t0, %[param]
-        \\addi a0, t0, 5
+    asm volatile ("addi %[result], %[param], 5"
         : [result] "=r" (result),
         : [param] "r" (param),
     );
     return result;
 }
 
-fn sys_write() usize {
-    var msg = "Hello world\n"; // Yazılacak mesaj
-    var len = msg.len; // Mesajın uzunluğu
-    var fd: usize = 0x01; // stdout (file descriptor)
-    var syscall_number: usize = 63; // write sistem çağrısı numarası
-    const ret: usize = 0;
-
-    asm volatile (
-        \\mv a0, %[fd]        // a0 = fd (stdout)
-        \\mv a1, %[msg]       // a1 = msg (yazılacak mesajın adresi)
-        \\mv a2, %[len]       // a2 = len (mesajın uzunluğu)
-        \\mv a7, %[syscall_number] // a7 = sistem çağrısı numarası (write = 63)
-        \\ecall               // ecll: sistem çağrısını gerçekleştir
-        // Sistem çağrısının dönüş değeri
-        : [fd] "r" (fd), // file descriptor (input)
-          [msg] "r" (msg), // mesajın bellekteki adresi (input)
-          [len] "r" (len), // mesajın uzunluğu (input)
-          [syscall_number] "r" (syscall_number), // sistem çağrısı numarası (input)
-
+fn asm_sum(x: usize, y: usize) usize {
+    var result: usize = 0;
+    asm volatile ("add %[t0], %[x], %[y]"
+        : [t0] "=r" (result),
+        : [x] "r" (x),
+          [y] "r" (y),
     );
+    return result;
+}
 
-    return ret;
+fn sys_write() usize {
+    const msg = "Hello world\n"; //
+    const len = msg.len; //
+    const fd: usize = 0x01; // stdout (file descriptor)
+    const syscall_number: usize = 64; // write
+
+    return asm volatile ("ecall" // ecll: sistem çağrısını gerçekleştir
+        // Sistem çağrısının dönüş değeri
+        : [ret] "={a0}" (-> usize),
+        : [fd] "{a0}" (fd), // file descriptor (input)
+          [msg] "{a1}" (@intFromPtr(msg)),
+          [len] "{a2}" (len),
+          [syscall_number] "{a7}" (syscall_number),
+        : "a0", "a1", "a2", "a7"
+    );
 }
 
 pub fn main() !void {
     const a2: usize = sys_write();
+    const sum: usize = asm_sum(3, 4); // 7
+    const example: usize = asm_example();
+    std.debug.print("{d}\n", .{example});
+    std.debug.print("{d}\n", .{sum});
     std.debug.print("{d}\n", .{a2});
 }
